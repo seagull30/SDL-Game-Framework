7dbca91b182a6dd6af053dc1143e81d278caaa29
seagull30 <minsang22@gmail.com>
2022-05-20 15:32:20 +0900

    csv파일 양식변경

--- SDL Game Framework/SDL Game Framework/Config.h
+++ SDL Game Framework/SDL Game Framework/Config.h
@@ -2,8 +2,8 @@
 #pragma once
 
 // 창의 크기
-#define WINDOW_WIDTH	1080
-#define WINDOW_HEIGHT	720
+#define WINDOW_WIDTH	1920
+#define WINDOW_HEIGHT	1080
 
 // 애셋 경로
 #define IMAGE_DIRECTORY	"Asset/Image"
@@ -20,3 +20,21 @@
 #define CLEAR_COLOR_G	255
 #define CLEAR_COLOR_B	255
 #define CLEAR_COLOR_A	255
+
+
+
+#define TEXT1_FONT "aB.ttf"
+#define TEXT1_FONT_SIZE 23 * ONE_POINT_FIVE
+#define TEXT1_POS_X 11
+#define TEXT1_POS_Y 50
+
+#define TEXT2_FONT TEXT1_FONT
+#define TEXT2_FONT_SIZE 26* ONE_POINT_FIVE
+#define TEXT2_POS_X 960
+#define TEXT2_POS_Y 850
+
+#define TEXT3_FONT "aL.ttf"
+#define TEXT3_POS_X TEXT2_POS_X
+#define TEXT3_POS_Y 930
+
+#define ONE_POINT_FIVE	1.5
